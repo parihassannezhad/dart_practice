bc380546c4a08ee9736c6df0a3ad5a6916f76480
mr.mojtaba <mr.mojtaba47@gmail.com>
2023-11-29 21:59:51 +0330

    add some TODO

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,5 +1,162 @@
-import 'package:dart_practice/dart_practice.dart' as dart_practice;
+
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_practice.calculate()}!');
+  var a1 = [
+    700,
+    32,
+    964,
+    614,
+    896,
+    295,
+    324,
+    925,
+    415,
+    976,
+    869,
+    894,
+    247,
+    896,
+    788,
+    681,
+    396,
+    120,
+    475,
+    191,
+    431,
+    598,
+    351,
+    538,
+    975,
+    47,
+    125,
+    769,
+    580,
+    626,
+    76,
+    803,
+    821,
+    55,
+    702,
+    2,
+    109,
+    730,
+    313,
+    326,
+    114,
+    499,
+    990,
+    276,
+    432,
+    385,
+    40,
+    906,
+    277,
+    36,
+    778,
+    545,
+    874,
+    102,
+    742,
+    570,
+    280,
+    270,
+    908,
+    509,
+    172,
+    326,
+    705,
+    502,
+    604,
+    628,
+    935,
+    947,
+    574,
+    406,
+    142,
+    850,
+    283,
+    224,
+    929,
+    84,
+    409,
+    696,
+    277,
+    207,
+    701,
+    443,
+    724,
+    443,
+    502,
+    427,
+    527,
+    99,
+    784,
+    363,
+    235,
+    331,
+    229,
+    780,
+    843,
+    478,
+    853,
+    25,
+    617,
+    38
+  ];
+  List<String> members = [
+    "Ali Akbari",
+    "Sara Hosseini",
+    "Mohammad Nori",
+    "Mahdiye Rostami",
+    "Javad Shojai",
+    "Fateme Rahimi",
+    "Maryam Khademi",
+    "Amirhossein Razavi",
+    "Saeed Hosseini",
+    "Safora Karimi",
+    "Pegah Ghasemi",
+    "Hamidreza Sadeghi",
+    "Morteza Rahmani",
+    "Narges Abedi",
+    "Hossein Shirazi",
+    "Mansoureh Moradi",
+    "Emad Bakhshayesh",
+    "Shahab Hosseini",
+    "Mahsa Mahmoudi",
+    "Ahmadreza Ghasemi"
+  ];
+
+  //TODO 1
+  /// pls write  function void and recursive code for sum all number in  array [a1]
+
+  //TODO 2
+  // pls write  function void and recursive code for sum all odd number in  array [a1]
+
+  //TODO 3
+  // pls write  function void and recursive code for sum all Prime Number in  array [a1]
+
+  //TODO 4
+  //pls write function  void and recursive for Abbreviation with UpperCase in array[members]
+  //Sara Hosseini => SH
+
+  //TODO 5
+  /* عدد آرمسترانگ
+ //pls write function
+
+  عدد آرامسرانگ عددی است که اگر اجزای آن به توان طول عددمون کنیم و جمع کنیم باز هم همان عدد را
+  بهمون میده!
+  مثال :  153
+  1^3 + 5^3 + 3^3 =153
+  (  ☝️این عدد آرمسترانگ است)
+
+  حالا چگونه  اعداد آرامسترانگ بین 1 تا 10,000
+  را پیدا کنیم؟؟*/
+
+
+  //TODO 6
+  ////pls write function  void and recursive for this Question
+  // we want sum all digit number like 123 => 1+2+3 = 6
+
+
+
 }
