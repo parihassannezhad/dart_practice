76882eb607a270bb6155bc51618c22a55a49428e
p.hassannezhad <parihassannezhad@gmail.com>
2023-12-02 20:10:05 +0330

    1402.9.12

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,4 +1,4 @@
-
+import 'dart:math';
 
 void main(List<String> arguments) {
   var a1 = [
@@ -125,22 +125,172 @@ void main(List<String> arguments) {
     "Mahsa Mahmoudi",
     "Ahmadreza Ghasemi"
   ];
-
-  //TODO 1
-  /// pls write  function void and recursive code for sum all number in  array [a1]
-
+//TODO 1
+  print(sumNumber(a1));
+  sumNumber2(a1);
   //TODO 2
-  // pls write  function void and recursive code for sum all odd number in  array [a1]
-
+  print(SumOddNumber(a1));
+  SumOddNumber2(a1);
   //TODO 3
-  // pls write  function void and recursive code for sum all Prime Number in  array [a1]
-
+  print(sumPrimeNember(a1));
+  sumPrimeNumber2(a1);
   //TODO 4
-  //pls write function  void and recursive for Abbreviation with UpperCase in array[members]
-  //Sara Hosseini => SH
+  abbreviationName(members);
+  print(abbreviationName2(members));
+  NumberPlus(numbersString);
+  // ArmNumber();
+}
 
-  //TODO 5
-  /* عدد آرمسترانگ
+//TODO 1/1
+/// pls write  function void and recursive code for sum all number in  array [a1]
+
+int sumNumber(List<int> a1) {
+  var sum = 0;
+  for (var number in a1) {
+    sum += number;
+  }
+  return sum;
+}
+
+//TODO 1/2
+void sumNumber2(List<int> a1) {
+  var sum = 0;
+  for (var number in a1) {
+    sum += number;
+  }
+  print(sum);
+}
+
+//TODO 2/1
+// pls write  function void and recursive code for sum all odd number in  array [a1]
+
+int SumOddNumber(List<int> a1) {
+  var sum = 0;
+
+  for (var number in a1) {
+    if (number % 2 == 0) {
+      sum += number;
+    }
+  }
+  return sum;
+}
+
+//TODO 2/2
+void SumOddNumber2(List<int> a1) {
+  var sum = 0;
+  for (var number in a1) {
+    if (number % 2 == 0) {
+      sum += number;
+    }
+  }
+  print(sum);
+}
+
+//TODO 3/1
+// pls write function void and recursive code for sum all Prime Number in  array [a1]
+
+bool isPrimeNumber(int number) {
+  if (number < 2) {
+    return false;
+  }
+  for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int sumPrimeNember(List<int> a1) {
+  var sum = 0;
+  for (var number in a1) {
+    if (isPrimeNumber(number)) {
+      sum += number;
+    }
+  }
+  return sum;
+}
+
+//TODO 3/2
+void sumPrimeNumber2(List<int> a1) {
+  var sum = 0;
+  for (var number in a1) {
+    if (isPrimeNumber(number)) {
+      sum += number;
+    }
+  }
+  print(sum);
+}
+
+//TODO 4/1
+//pls write function  void and recursive for Abbreviation with UpperCase in array[members]
+//Sara Hosseini => SH
+void abbreviationName(List<String> members) {
+  for (var name in members) {
+    final firstName = name.split(' ').firstOrNull.toString();
+    final lastNAme = name.split(' ').lastOrNull.toString();
+    final f1 = firstName.split('').firstOrNull;
+    final f2 = lastNAme.split('').firstOrNull;
+
+    print('$name=> $f1$f2');
+  }
+}
+
+//TODO 4/2
+String? abbreviationName2(List<String> members) {
+  String? reasult;
+  for (var name in members) {
+    final firstName = name.split(' ').firstOrNull.toString();
+    final lastNAme = name.split(' ').lastOrNull.toString();
+    final f1 = firstName.split('').firstOrNull;
+    final f2 = lastNAme.split('').firstOrNull;
+    final reasult = print('$name=> $f1$f2');
+  }
+  return reasult;
+}
+
+//TODO 6/1
+////pls write function  void and recursive for this Question
+// we want sum all digit number like 123 => 1+2+3 = 6
+
+final numbersInt = 150036;
+final numbersString = numbersInt.toString();
+
+void NumberPlus(String numbersString) {
+  final seperateNumber = numbersString.split('').toString();
+  final replaceNumber = seperateNumber.replaceAll(',', ' +');
+  final answer = replaceNumber.replaceAll('[', '');
+  final answer2 = answer.replaceAll(']', '');
+  int sum = 0;
+  for (int i = 0; i < numbersString.length; i++) {
+    sum += int.parse(numbersString[i]);
+  }
+  print('$answer2 => $sum');
+}
+
+
+
+// void ArmNumber(){
+//   for (int num = 1; num <= 10000; num++) {
+//     var numString = num.toString();
+//     int sum = 0;
+//     int num2 = num;
+//
+//     while (num2 > 0) {
+//       int digit = num2 % 10;
+//       sum += pow(digit, numString.length).toInt();
+//       num2 ~/= 10;
+//     }
+//
+//     if (num == sum) {
+//       print(num);
+//     }
+//   }
+// }
+
+
+//TODO 5
+/* عدد آرمسترانگ
  //pls write function
 
   عدد آرامسرانگ عددی است که اگر اجزای آن به توان طول عددمون کنیم و جمع کنیم باز هم همان عدد را
@@ -153,10 +303,3 @@ void main(List<String> arguments) {
   را پیدا کنیم؟؟*/
 
 
-  //TODO 6
-  ////pls write function  void and recursive for this Question
-  // we want sum all digit number like 123 => 1+2+3 = 6
-
-
-
-}
